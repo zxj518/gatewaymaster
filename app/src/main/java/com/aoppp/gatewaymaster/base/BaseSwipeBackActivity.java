a5d3aa22bea8e2499b7c97b9bc11f913b41b643e
zxj518 <zxj518@gmail.com>
2015-07-26 23:35:37 +0800

    add main activity

---
+++ app/src/main/java/com/aoppp/gatewaymaster/base/BaseSwipeBackActivity.java
@@ -0,0 +1,53 @@
+package com.aoppp.gatewaymaster.base;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.aoppp.gatewaymaster.swipeback.SwipeBackActivityBase;
+import com.aoppp.gatewaymaster.swipeback.SwipeBackActivityHelper;
+import com.aoppp.gatewaymaster.swipeback.SwipeBackLayout;
+import com.aoppp.gatewaymaster.swipeback.Utils;
+
+
+public abstract class BaseSwipeBackActivity extends BaseActivity implements SwipeBackActivityBase {
+
+    private SwipeBackActivityHelper mHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mHelper = new SwipeBackActivityHelper(this);
+        mHelper.onActivityCreate();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mHelper.onPostCreate();
+    }
+
+    @Override
+    public View findViewById(int id) {
+        View v = super.findViewById(id);
+        if (v == null && mHelper != null)
+            return mHelper.findViewById(id);
+        return v;
+    }
+
+    @Override
+    public SwipeBackLayout getSwipeBackLayout() {
+        return mHelper.getSwipeBackLayout();
+    }
+
+    @Override
+    public void setSwipeBackEnable(boolean enable) {
+        getSwipeBackLayout().setEnableGesture(enable);
+    }
+
+    @Override
+    public void scrollToFinishActivity() {
+        Utils.convertActivityToTranslucent(this);
+        getSwipeBackLayout().scrollToFinishActivity();
+    }
+}
